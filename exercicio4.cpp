78c3b35934e5ec5e2e2f9cd48811b9aef795e3c9
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-18 23:07:43 +0000

    feito

---
+++ exercicio4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char palavra[10];
+
+    std:: cout << "Digite a palavra: ";
+    std::cin >> palavra;
+    std::cout << " "<< palavra<<std::endl;
+    
+
+
+    return false;
+}
---
+++ exercicio5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    char frase[20];
+
+    std:: cout << "Digite a frase: ";
+    std::cin.getline(frase,20);
+    std::cout << " "<< frase<<std::endl;
+    
+
+
+    return false;
+}
