5cc69269edf65e1caec584e75a54e8dee6a4ecf7
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-19 00:05:39 +0000

    feito

---
+++ exercicio6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numeros[10] = {1,2,3,4,5,6,7,8,9,10};
+
+   std::cout << "Números pares:\n";
+    if (numeros[0] % 2 == 0) 
+        std::cout << numeros[0] << "\n"; 
+    if (numeros[1] % 2 == 0) 
+        std::cout << numeros[1] << "\n"; 
+    if (numeros[3] % 2 == 0)
+        std::cout << numeros[3] << "\n"; 
+    if (numeros[5] % 2 == 0) 
+        std::cout << numeros[5] << "\n"; 
+    if (numeros[7] % 2 == 0) 
+        std::cout << numeros[7] << "\n"; 
+    if (numeros[9] % 2 == 0) 
+        std::cout << numeros[9] << "\n"; 
+
+    cout << "sadfdsafsdaf" << numeros[0];
+    return false;
+}
---
+++ exercicio7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+     std::string frutas[5] = {"banana", "melão", "melancia", "tomate", "uva"};
+
+    std::cout << frutas[2] << std::endl;
+    
+    return 0;
+}
